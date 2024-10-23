e90c8894e140099347698bbe522c31490d58475e
Shehan Peiris <shehanmaleesha4884@gmail.com>
2024-10-23 10:38:14 +0530

    Report Generator Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from "./common/navbar/navbar.component";
+import { FooterComponent } from "./common/footer/footer.component";
+import { SideBarComponent } from "./common/side-bar/side-bar.component";
+import { DashboardComponent } from "./reports/dashboard/dashboard.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, SideBarComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
